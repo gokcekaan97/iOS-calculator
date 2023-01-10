c7bf6be5432dc75811b16b8409ce7657b48caa87
Kaan Gokcek <48089870+gokcekaan97@users.noreply.github.com>
2023-01-10 16:36:11 +0300

    init

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -8,7 +8,65 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var screen: UITextField!
+    var tempInteger : Int = 0
+    @IBAction func oneTo(_ sender: Any) {
+        screen.insertText("1")
+    }
+    @IBAction func twoTo(_ sender: Any) {
+        screen.insertText("2")
+    }
+    @IBAction func threeTo(_ sender: Any) {
+        screen.insertText("3")
+    }
+    @IBAction func fourTo(_ sender: Any) {
+        screen.insertText("4")
+    }
+    @IBAction func fiveTo(_ sender: Any) {
+        screen.insertText("5")
+    }
+    @IBAction func sixTo(_ sender: Any) {
+        screen.insertText("6")
+    }
+    @IBAction func sevenTo(_ sender: Any) {
+        screen.insertText("7")
+    }
+    @IBAction func eigthTo(_ sender: Any) {
+        screen.insertText("8")
+    }
+    @IBAction func nineTo(_ sender: Any) {
+        screen.insertText("9")
+    }
+    @IBAction func zeroTo(_ sender: Any) {
+        screen.insertText("0")
+    }
+    @IBAction func equals(_ sender: Any) {
+        var result : String = screen.text!
+        var a = 0
+        let x = result.components(separatedBy: CharacterSet(charactersIn: "+-/*"))
+        screen.text?.removeAll()
+        for num in x{
+            a += Int(num)!
+        }
+        screen.insertText(String(a))
+    }
+    @IBAction func clears(_ sender: Any) {
+        screen.text?.removeAll()
+    }
+    @IBAction func plus(_ sender: Any) {
+        screen.insertText("+")
+    }
+    @IBAction func minus(_ sender: Any) {
+        screen.insertText("-")
+    }
+    @IBAction func division(_ sender: Any) {
+        screen.insertText("/")
+    }
+    @IBAction func multiply(_ sender: Any) {
+        screen.insertText("*")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
